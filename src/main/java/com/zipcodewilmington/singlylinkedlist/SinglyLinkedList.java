ca8b8738cc249439e0fb872f20627791193c9a20
Caleb Powell <calebpowell@zipcoders-MacBook-Pro-2.local>
2019-06-22 19:27:08 -0400

    SinglyLinkedList add method and test added

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,5 +3,44 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T>{
+    private class Node{
+        private Node next;
+        private T content;
+
+        public Node(T content){
+            this.content = content;
+        }
+
+        public T getContent() {
+            return content;
+        }
+    }
+
+    private Node start;
+    private Node end;
+    private Integer position;
+
+    public void add(T content){
+        if(start == null){
+            start = new Node(content);
+            end = start;
+        } else {
+            end.next = new Node(content);
+        }
+    }
+
+    public T get(Integer index){
+        if(start == null)return null;
+        position = 0;
+        Node current = start;
+        while(position < index){
+            if(current.next == null)return null;
+            else {
+                current = current.next;
+                position++;
+            }
+        }
+        return current.getContent();
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,21 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+    @Test
+    public void add() {
+        SinglyLinkedList<Integer> sll = new SinglyLinkedList<Integer>();
+        Integer expected = 665;
+        sll.add(expected);
+
+        Integer actual = sll.get(0);
+
+        Assert.assertEquals(expected, actual);
+    }
 }
